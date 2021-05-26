99bdf53501d2f1fea0b6d1aeb7c84e3826e07770
Aaron-lv <674560606@qq.com>
2021-05-26 13:51:20 +0800

    更新脚本

--- updateTeam/proc_file.sh
+++ updateTeam/proc_file.sh
@@ -27,35 +27,41 @@ fi
 ##京喜工厂自动开团
 if [ $jd_jxFactoryCreateTuan_ENABLE = "Y" ]; then
     echo "# 京喜工厂自动开团" >> $mergedListFile
-    echo "0 0-4/1 * * * cd /scripts && node jd_jxFactoryCreateTuan.js >> logs/jd_jxFactoryCreateTuan.log 2>&1 && upload" >> $mergedListFile
+    echo "0 0-4/1 * * * cd /scripts && node jd_jxFactoryCreateTuan.js >> logs/jd_jxFactoryCreateTuan.log 2>&1 && upload >> logs/upload_jd_jxFactoryCreateTuan.log 2>&1" >> $mergedListFile
 fi
 
 ##更新抢京豆邀请码
 if [ $jd_updateBeanHome_ENABLE = "Y" ]; then
     echo "# 更新抢京豆邀请码" >> $mergedListFile
-    echo "0 0,2 * * * cd /scripts && node jd_updateBeanHome.js >> logs/jd_updateBeanHome.log 2>&1 && upload" >> $mergedListFile
+    echo "0 0,2 * * * cd /scripts && node jd_updateBeanHome.js >> logs/jd_updateBeanHome.log 2>&1 && upload >> logs/upload_jd_updateBeanHome.log 2>&1" >> $mergedListFile
 fi
 
 ##京东签到领现金
 if [ $jd_updateCash_ENABLE = "Y" ]; then
     echo "# 京东签到领现金" >> $mergedListFile
-    echo "0 0 * * * cd /scripts && node jd_updateCash.js >> logs/jd_updateCash.log 2>&1 && upload" >> $mergedListFile
+    echo "0 0 * * * cd /scripts && node jd_updateCash.js >> logs/jd_updateCash.log 2>&1 && upload >> logs/upload_jd_updateCash.log 2>&1" >> $mergedListFile
 fi
 
 ##京喜财富岛
 if [ $jd_updateCfd_ENABLE = "Y" ]; then
     echo "# 京喜财富岛" >> $mergedListFile
-    echo "0-3/1 0,7 * * * cd /scripts && node jd_updateCfd.js >> logs/jd_updateCfd.log 2>&1 && upload" >> $mergedListFile
+    echo "0-3/1 0,7 * * * cd /scripts && node jd_updateCfd.js >> logs/jd_updateCfd.log 2>&1 && upload >> logs/upload_jd_updateCfd.log 2>&1" >> $mergedListFile
+fi
+
+##全民开红包
+if [ $jd_updateRed_ENABLE = "Y" ]; then
+    echo "# 全民开红包" >> $mergedListFile
+    echo "0 0,4 * * * cd /scripts && node jd_updateRed.js >> logs/jd_updateRed.log 2>&1 && upload >> logs/upload_jd_updateRed.log 2>&1" >> $mergedListFile
 fi
 
 ##更新东东小窝邀请码
 if [ $jd_updateSmallHome_ENABLE = "Y" ]; then
     echo "# 更新东东小窝邀请码" >> $mergedListFile
-    echo "0 0 * * * cd /scripts && node jd_updateSmallHome.js >> logs/jd_updateSmallHome.log 2>&1 && upload" >> $mergedListFile
+    echo "0 0 * * * cd /scripts && node jd_updateSmallHome.js >> logs/jd_updateSmallHome.log 2>&1 && upload >> logs/upload_jd_updateSmallHome.log 2>&1" >> $mergedListFile
 fi
 
 ##赚京豆小程序
 if [ $jd_zzUpdate_ENABLE = "Y" ]; then
     echo "# 赚京豆小程序" >> $mergedListFile
-    echo "*/20 0-2/1 * * * cd /scripts && node jd_zzUpdate.js >> logs/jd_zzUpdate.log 2>&1 && upload" >> $mergedListFile
+    echo "*/20 0-2/1 * * * cd /scripts && node jd_zzUpdate.js >> logs/jd_zzUpdate.log 2>&1 && upload >> logs/upload_jd_zzUpdate.log 2>&1" >> $mergedListFile
 fi
